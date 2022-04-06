e96f8eb4fd886c95d3925fbdc7ed06b224f576f1
bnaebain <bnaebain@icloud.com>
2022-04-06 08:34:46 -0500

    API requests: Done | TBD: Input/Functional

--- src/converter.js
+++ src/converter.js
@@ -42,30 +42,46 @@ class Symbols{
         //let symb = Object.keys(body.symbols);
         return await result.json();
         //return symb;
-        
-    }
-
-    // Validation
-    checker(){
-        const l = this.list;
-        // Will build in iterator to check input symbols v list of supported symbols
-        console.log(l);
-        return l;
-       
     }
 }
+
+// Validation
+function checker(list, f_str, t_str){
+    for (let i in list) {
+        if (list.indexOf(f_str)) {
+            console.log("Origin ISO code ",f_str," is valid");
+            break;
+        }
+        else console.log("Origin input ",f_str," is not valid ISO code.")
+    }
+    for (let i = 0; i < list.length; i++) {
+        if (list.indexOf(t_str)) {
+            console.log("Destination ISO code ",t_str," is valid");
+            break;
+        }
+        else console.log("Destination input ",t_str," is not valid ISO code.")
+    }
+    return;
+}
+
+
 let input_f = 'USD';
-let input_t = 'EUR';
+let input_t = 'ISK';
 let input_a = 1200;
 
 let x = new Symbols(input_f, input_t);
 x.symbols.then((result)=> {
-   // console.log("X Symbols", result)
+    ///console.log("X Symbols", result.symbols);
+    let list_s = Object.keys(result.symbols);
+    return checker(list_s, input_f, input_t);
 });
+
+
+
 
 let y = new Conversion(input_f, input_t, input_a);
 y.result.then((result)=> {
-    console.log("y result", result)
+    console.log("Converted Amount is: ", result.result)
 })
 
 
