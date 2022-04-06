4c122248da06f9237d5ba414d61f9e1755f107d6
bnaebain <bnaebain@icloud.com>
2022-04-06 08:48:58 -0500

    Functional Programing?

--- src/converter.js
+++ src/converter.js
@@ -1,51 +1,25 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-class Conversion{
-    constructor(from, to, amount) {
-        this.f = from;
-        this.t = to;
-        this.a = amount;
-    }
 
-    // Getter
-    get result() {
-        return this.converter();
-    }
-
-    // Method
-    async converter() {
-        let url = ["https://data.fixer.io/api/convert?access_key=5224cc818d14737db40e2077cf38b610", 
-            "&from=", this.f, "&to=", this.t, "&amount=", this.a];
-        let url_string = url.join('');
-        let result = await fetch(url_string, {type: 'json'})
-        //if (err) {return console.log(err)};
-        return await result.json();
-    }
+async function converter(f, t, a) { // Converter Endpoint
+    let url = ["https://data.fixer.io/api/convert?access_key=5224cc818d14737db40e2077cf38b610", 
+        "&from=", f, "&to=", t, "&amount=", a];
+    let url_string = url.join('');
+    let result = await fetch(url_string, {type: 'json'})
+    //if (err) {return console.log(err)};
+    return await result.json();
 }
 
-class Symbols{
-    constructor(from, to) {
-        this.f = from;
-        this.t = to;
-    }
 
-    // Getter
-    get symbols() {
-        return this.list();
-    }
-
-    // Method: Pulls list of supported symbols from API
-    async list(){
-        let url = 'https://data.fixer.io/api/symbols?access_key=5224cc818d14737db40e2077cf38b610';
-        let result = await fetch(url, {type: 'json'})
-        //if (err) {return console.log(err)};
-        //let symb = Object.keys(body.symbols);
-        return await result.json();
-        //return symb;
-    }
+async function list(){ // Supported Symbols Endpoint
+    let url = 'https://data.fixer.io/api/symbols?access_key=5224cc818d14737db40e2077cf38b610';
+    let result = await fetch(url, {type: 'json'})
+    //if (err) {return console.log(err)};
+    return await result.json();
 }
 
-// Validation
+
+// Validation of input
 function checker(list, f_str, t_str){
     for (let i in list) {
         if (list.indexOf(f_str)) {
@@ -69,22 +43,19 @@ let input_f = 'USD';
 let input_t = 'ISK';
 let input_a = 1200;
 
-let x = new Symbols(input_f, input_t);
-x.symbols.then((result)=> {
+let x = list();
+x.then((result)=> {
     ///console.log("X Symbols", result.symbols);
     let list_s = Object.keys(result.symbols);
     return checker(list_s, input_f, input_t);
 });
 
-
-
-
-let y = new Conversion(input_f, input_t, input_a);
-y.result.then((result)=> {
+let y = converter(input_f, input_t, input_a);
+y.then((result)=> {
     console.log("Converted Amount is: ", result.result)
 })
 
 
 
 
-module.exports = {Conversion};
+///module.exports = {Conversion};
