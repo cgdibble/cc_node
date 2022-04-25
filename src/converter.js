bdf2e1432b35f4a597975325e32af27891c7cda2
bnaebain <bnaebain@icloud.com>
2022-04-25 11:35:40 -0500

    Added Tests, Attempted to make Inquirer Async/Await

--- src/converter.js
+++ src/converter.js
@@ -1,6 +1,6 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = require("node-fetch")
 const inquirer = require('inquirer');
-const index = require('./index.js');
+
 
 async function getConvertedAmount(from, to, amt) { // Converter Endpoint www.fixer.io
     let url = ["https://data.fixer.io/api/convert?access_key=5224cc818d14737db40e2077cf38b610", 
@@ -9,6 +9,8 @@ async function getConvertedAmount(from, to, amt) { // Converter Endpoint www.fix
     let result = await fetch(url_string, {type: 'json'})
     //if (err) {return console.log(err)};
     let full_result = await result.json();
+    console.log('result', result);
+    ///console.log('full_result.result', typeof full_result.result);
     return full_result.result;
 }
 
@@ -25,7 +27,7 @@ async function isoCheck(list, f_str, t_str, amount){ // Validation of input
     let destinationValid = list.indexOf(t_str);
     if (originValid > 0 && destinationValid > 0) {
         let c_amt = await getConvertedAmount(f_str, t_str, amount);
-                    console.log("Converted Amount is: ", c_amt.toFixed(2));
+                    console.log("Converted Amount is: ", c_amt.toFixed(2), t_str);
                     return startProgram();
                 }
     if (originValid < 0 && destinationValid < 0) {
@@ -46,78 +48,73 @@ function getIsoFromCurrencyName(currenciesByIsoCodes, currencyName){
     return Object.keys(currenciesByIsoCodes).find(key => currenciesByIsoCodes[key] === currencyName);
 }
 
-const startProgram = () => {
+const startProgram = async () => {
     let symbolList = getSymbolList();
     symbolList.then((result)=> {
         dict = result.symbols;
         countryList = Object.values(result.symbols);
         symbList = Object.keys(result.symbols);
     })
-    inquirer
-        .prompt([
-            {
+    const answer = await inquirer.prompt({
               name: 'greeting',
               message: 'What would you like to do?',
               type: 'list',
               choices: ['Look up a currency ISO code', 'Convert a currency', 'Exit']
-            },
-        ])
-        .then(answer =>  {
-            console.log(answer.greeting);
-            if (answer.greeting == 'Look up a currency ISO code') {
-                return getISO();
-            }
-            else if (answer.greeting == 'Convert a currency') {
-                return startConversion();
-            } 
-            else return;
-        });
-}
-const getISO = () => {
-    inquirer
-        .prompt([
-            {
-                name: 'choose_iso',
-                message: 'What currency would you like?',
-                type: 'list',
-                choices: countryList,
-            },
-        ])
-        .then(answer => {
-            console.log(answer.choose_iso);
-            let iso = getIsoFromCurrencyName(dict, answer.choose_iso);
-            console.log("Your currency ISO code is ", iso);
-            return startProgram();
-        })
+    })
+    
+    if (answer.greeting === 'Look up a currency ISO code') {
+        const getIso = await inquirer.prompt(getISO(countryList))
+        const iso = getIsoFromCurrencyName(dict, getIso.choose_iso);
+        console.log("Your currency ISO code is ", iso);
+        return startProgram;
+    }
+    if (answer.greeting === 'Convert a currency') {
+        let inq = startConversion();
+        const conversion_from = await inquirer.prompt(inq[0])
+        const conversion_to = await inquirer.prompt(inq[1])
+        const conversion_amt = await inquirer.prompt(inq[2])
+        return isoCheck(symbList, conversion_from, conversion_to, conversion_amt);
+    } else {
+        return
+    }
 }
 
-const startConversion = () => {
-    inquirer
-        .prompt([
-            {
-                name: 'from_country',
-                message: 'Please enter the ISO code of your origin currency.',
-                type: 'input',
-            },
-            {
-                name: 'to_country',
-                message: 'Please enter the ISO code of your destination currency.',
-                type: 'input',
-            },
-            {
-                name: 'amount',
-                message: 'Please enter the amount to convert.',
-                type: 'number',
-            },
-        ])
-        .then(answer => {
-            let valid = isoCheck(symbList, answer.from_country, answer.to_country, answer.amount);
-        })
+const getISO = (countryList) => {
+    return {
+        name: 'choose_iso',
+        message: 'What currency would you like?',
+        type: 'list',
+        choices: countryList,
+    }
+}
+
+
+const startConversion = async () => {
+    const fromC = {
+        name: 'from_country',
+        message: 'Please enter the ISO code of your origin currency.',
+        type: 'input',
+    }
+    const toC = {
+        name: 'to_country',
+        message: 'Please enter the ISO code of your destination currency.',
+        type: 'input',
+    }
+    const amt = {
+        name: 'amount',
+        message: 'Please enter the amount to convert.',
+        type: 'number',
+    }
+    return await [fromC, toC, amt]
 }
 
 module.exports = {
-    getIsoFromCurrencyName,
-    startProgram,
+    getIsoFromCurrencyName, //
+    startProgram, //
     getSymbolList,
+    getConvertedAmount, //
+    isoCheck, //
+    startConversion,
+    getISO
 }
 
--- src/converter.test.js
+++ src/converter.test.js
@@ -1,7 +1,16 @@
-console.log(`getIsoFromCurrencyName`, getIsoFromCurrencyName);
-const { getIsoFromCurrencyName } = require("./converter")
+const { getIsoFromCurrencyName, getConvertedAmount, isoCheck, startProgram, startConversion, getSymbolList, getISO } = require("./converter");
+
+const fetch = require("node-fetch")
+jest.mock("node-fetch")
+
+const inquire = require("inquirer")
+jest.mock('inquirer', () => {
+  return { prompt: jest.fn() };
+});
+
 describe('identify test', () => {
-    describe.only('getISOfromCurrencyName()', () => {
+    console.log(`getIsoFromCurrencyName`, getIsoFromCurrencyName);
+    describe('getISOfromCurrencyName()', () => {
         it('should return the USA ISO Code when given the appropriate currency', () => {
             const currencyNameByIsoCodes ={
                 "USD" : "United States Dollar"
@@ -19,24 +28,63 @@ describe('identify test', () => {
             expect(result).toBe("CAD")
         })
     })
-})
-
-
-console.log('getConvertedAmount', getConvertedAmount);
-const { getConvertedAmount } = require("./converter")
-describe('conversion test', () => {
-    describe.only('getConvertedAmount()', () => {
-        it('should connect to the Fixer.io API and return a float object for the converted amount', () => {
-            [from, to, amount] = ['USD', 'EUR', 1000];
-            const result = getConvertedAmount(from, to, amount)
-            expect(result).toBeInstanceOf(Number)
+    console.log('isoCheck', isoCheck)
+    describe('isoCheck()', () => {
+        const objectList = ["USD", "CAD", "EUR"]
+        const amount = 1000
+        it('should validate both inputs are part of the object array returned by the API', async () => {
+            const f_country = "USD"
+            const t_country = "CAD"
+            const result = await isoCheck(objectList, f_country, t_country, amount)
+            expect(startProgram).toHaveBeenCalled;
         })
-        it('should not lead to errors', () => {
-            [from, to, amount] = ['USD', 'EUR', 1000];
-            const result = getConvertedAmount(from, to, amount)
-            expect(getErrors()).toBeFalsy();
+        it('should validate that neither input is in the object array returned by the API', () => {
+            const f_country = "NaC"            
+            const t_country = "NaC"
+            const result = isoCheck(objectList, f_country, t_country, amount)
+            expect(startConversion).toHaveBeenCalled;
+        })
+        it('should validate that the from country is not in the object array returned by the API', () => {
+            const f_country = "NaC"            
+            const t_country = "EUR"
+            const result = isoCheck(objectList, f_country, t_country, amount)
+            expect(startConversion).toHaveBeenCalled;
+        })
+        it('should validate that the to country is not in the object array returned by the API', () => {
+            const f_country = "USD"            
+            const t_country = "NaC"
+            const result = isoCheck(objectList, f_country, t_country, amount)
+            expect(startConversion).toHaveBeenCalled;
         })
     })
 })
 
 
+describe('conversion test', () => {
+    console.log('getConvertedAmount', getConvertedAmount);
+    describe('getConvertedAmount()', () => {
+        it('should connect to the Fixer.io API and return a float object for the converted amount', async () => {
+            [from, to, amount] = ['USD', 'EUR', 1000];
+            const expectation = "1234.667"
+            fetch.mockResolvedValue({
+                json: jest.fn().mockResolvedValue({ result: expectation})
+            })
+            const result = await getConvertedAmount(from, to, amount)
+            expect(result).toEqual(expectation)
+        })
+    })
+})
+
+describe('inquirer test', () => {
+    console.log('startProgram', startProgram)
+    describe('startProgram()', () => {
+        it('should start the symbolList function to pull the data from the API', () => {
+            expect(getSymbolList).toHaveBeenCalled;
+        })
+        it ('should call a function based on the response', async () => {
+            inquirer.prompt.mockResolvedValueOnce('Start ISO');
+            const actual = await getAnswers('Look up a currency ISO code');
+            expect(actual).toBe('Start ISO');
+        })
+    })
+})
